f63bbd7245aafe89412f2f2de90dd1875ed383a2
takkun5 <takuya0215sai@gmail.com>
2015-08-08 06:25:50 +0000

    tainer signup edit

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,5 +1,26 @@
 class TrainersController < ApplicationController
+  def show
+    @trainer = Trainer.find(params[:id])  
+  end
+  
   def new
-   
+    @trainer = Trainer.new
+  end
+  
+  def create
+    @trainer = Trainer.new(trainer_params)
+    if @trainer.save
+       flash[:success] = "Welcome to the SportsHub!"
+       redirect_to @trainer
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def trainer_params
+    params.require(:trainer).permit(:name, :email, :age, :profile, :speciality, :sports_type, :place,  :password, 
+                                   :password_confirmation)
   end
 end
