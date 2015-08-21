ed7bf02fa67be5fb464c0d8526ba9f1be16c9499
takkun5 <takuya0215sai@gmail.com>
2015-08-21 07:47:55 +0000

    image edit

--- app/controllers/training_contents_controller.rb
+++ app/controllers/training_contents_controller.rb
@@ -1,5 +1,5 @@
 class TrainingContentsController < ApplicationController
-    before_action :logged_in_user, only: [:new, :create, :destroy,:edit, :update]
+    before_action :logged_in_user, only: [:new, :create, :destroy, :edit, :update]
     before_action :set_training_content, only: [:show, :edit, :update]
    
    def show
@@ -20,17 +20,20 @@ class TrainingContentsController < ApplicationController
     end
     
     def edit
+    @training_content = current_user.training_contents.find_by(id: params[:id])
     end
  
     def update
-    @training_content = current_user.training_contents.find_by(id: params[:id])
-    return redirect_to root_url if @training_content.nil?
-    @training_content.update
+    @training_content = current_user.training_contents.find_by(id: params[:id])    
+    if @training_content.update(training_content_params)
     flash[:success] = "プランを編集しました。"
     redirect_to request.referrer || root_url
-   else
-     render 'edit'
-   end
+    else
+        render 'edit'
+    end    
+    end
+   
+
    
     
     def destroy
@@ -45,10 +48,10 @@ class TrainingContentsController < ApplicationController
     
     private
     def training_content_params
-        params.require(:training_content).permit(:name, :content,  
+    params.require(:training_content).permit(:name, :content,  
                         :sports_type, :purpose, :lengths, :image)
-        
     end
+    
     
     def set_training_content
     @training_content = TrainingContent.find(params[:id])
